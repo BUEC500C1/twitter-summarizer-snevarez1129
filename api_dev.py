5bb1d27991413b9ab45ee607910082b8886fa331
Sebastian Nevarez <snevarez@bu.edu>
2020-02-09 22:15:11 -0500

    added support for searching airports by name

--- api_dev.py
+++ api_dev.py
@@ -9,7 +9,7 @@ api = Api(app)
 
 #Global Variables
 file_downloaded = 0
-name = []
+airport_name = []
 iata_code = []
 latitude = []
 longitude = []
@@ -38,7 +38,7 @@ def read_data():
     with open('airport_data.csv') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
-            name.append(row[3])
+            airport_name.append(row[3])
             iata_code.append(row[13])
             latitude.append(row[4])
             longitude.append(row[5])
@@ -47,6 +47,11 @@ def read_data():
 def get_data(code):
     iata_idx = iata_code.index(code)
     return iata_idx
+
+def get_data_by_name(name):
+    fixed_name = name.replace('-', ' ')
+    name_idx = airport_name.index(fixed_name)
+    return name_idx
 
 def get_weather(lat, lon):
     global API_KEY
@@ -84,7 +89,7 @@ def get_temp_data(lat, lon):
 def format_response(index):
     data = {
         'id': {
-            'name': name[index],
+            'name': airport_name[index],
             'iata_code': iata_code[index]
         },
         'coord': {
@@ -193,7 +198,7 @@ def format_response(index):
     return data
 
 #API Class Definitions
-class Airports(Resource):
+class IATA(Resource):
     def get(self, airport_code):
         #download() #get the airports csv file
         read_data() #read all the airport codes in the file
@@ -204,15 +209,20 @@ class Airports(Resource):
         data = format_response(index) #format api response
         return data, 201 #return data to the user
 
-# class CurrentWeather(Resource):
-#     def get(self, lon, lat):
-#         res = get_weather(lon, lat)
-#         print(res)
-#         return
+class Name(Resource):
+    def get(self, port_name):
+        #download() #get the airports csv file
+        read_data() #read all the airport codes in the file
+        index = get_data_by_name(port_name) #get the coordinates of the airport
+        res = get_weather(latitude[index], longitude[index]) #get weather based on coordinates using openweathermap api
+        read_weather(res) #get the data we want from the api response
+        get_temp_data(latitude[index], longitude[index]) #get forecast for next 24hrs
+        data = format_response(index) #format api response
+        return data, 202 #return data to the user
 
 #Resources
-api.add_resource(Airports, '/<airport_code>')
-#api.add_resource(CurrentWeather, '/<lon>/<lat>')
+api.add_resource(IATA, '/iata/<airport_code>')
+api.add_resource(Name, '/name/<port_name>')
 
 if __name__ == '__main__':
     app.run()
